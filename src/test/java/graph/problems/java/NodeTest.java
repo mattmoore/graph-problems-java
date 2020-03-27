6f88cfdaa2deda24098b85f76bdf4986fe3f4692
Matt Moore <matt@mattmoore.io>
2020-03-27 16:49:02 -0500

    Add fourth level test for BFS + DFS.

--- src/test/java/graph/problems/java/NodeTest.java
+++ src/test/java/graph/problems/java/NodeTest.java
@@ -19,24 +19,28 @@ class NodeTest {
     @Test void dfsTest() {
         var commandStructure = new Node("Command Structure");
         var general = commandStructure.addChild("General");
-        general.addChild("Sergeant");
-        general.addChild("Corporal");
+        var sergeant = general.addChild("Sergeant");
+        var corporal = general.addChild("Corporal");
+        var lieutenant = corporal.addChild("Lieutenant");
 
         assertEquals("Command Structure", DFS.search("Command Structure", commandStructure).value, "first level");
         assertEquals("General", DFS.search("General", commandStructure).value, "second level");
         assertEquals("Sergeant", DFS.search("Sergeant", commandStructure).value, "third level, first neighbor");
         assertEquals("Corporal", DFS.search("Corporal", commandStructure).value, "third level, second neighbor");
+        assertEquals("Lieutenant", DFS.search("Lieutenant", commandStructure).value, "fourth level");
     }
 
     @Test void bfsTest() {
         var commandStructure = new Node("Command Structure");
         var general = commandStructure.addChild("General");
-        general.addChild("Sergeant");
-        general.addChild("Corporal");
+        var sergeant = general.addChild("Sergeant");
+        var corporal = general.addChild("Corporal");
+        var lieutenant = corporal.addChild("Lieutenant");
 
         assertEquals("Command Structure", BFS.search("Command Structure", commandStructure).value, "first level");
         assertEquals("General", BFS.search("General", commandStructure).value, "second level");
-        assertEquals("Sergeant", BFS.search("Sergeant", commandStructure).value, "third level, first neighbor");
-        assertEquals("Corporal", BFS.search("Corporal", commandStructure).value, "third level, second neighbor");
+        assertEquals("Sergeant", BFS.search("Sergeant", commandStructure).value, "third level");
+        assertEquals("Corporal", BFS.search("Corporal", commandStructure).value, "third level");
+        assertEquals("Lieutenant", BFS.search("Lieutenant", commandStructure).value, "fourth level");
     }
 }
